0593136282cab6e4358cb1442c33e8ca1d0a99da
shaylahinkley <hinkleys@mail.gvsu.edu>
2019-12-03 18:20:13 -0500

    format

--- src/MySingleLinkedList.java
+++ src/MySingleLinkedList.java
@@ -1,24 +1,50 @@
 import java.io.Serializable;
-
+/***********************************************************************************************************************
+ * CIS 162 Project 4
+ * Class that controls the Linklist that is used within the GUI
+ *
+ * @author Keilani Bailey and Shayla Hinkley
+ * @version Project 2: October 2nd, 2019
+ **********************************************************************************************************************/
 public class MySingleLinkedList implements Serializable
 {
+    /** the top of the link list (beginning) */
     private Node top;
+
+    /** the tail of the link list (ending) */
     private Node tail;
+
+    /** the size of the linked list*/
     public int size;
 
+    /*******************************************************************************************************************
+     * Constructor method that creates a linked list
+     ******************************************************************************************************************/
     public MySingleLinkedList() {
         top = null;
         size = 0;
     }
 
+    /*******************************************************************************************************************
+     * Method that returns the size of the linked list
+     *
+     * @return size - an integer that represents the size of the linked list
+     ******************************************************************************************************************/
     public int size() {
         return size;
     }
 
-    public void clear () {
+    /*******************************************************************************************************************
+     * Method that clears the linked list
+     ******************************************************************************************************************/
+    public void clear() {
         top = null;
         size = 0;
     }
+
+    /*******************************************************************************************************************
+     * Method that adds an element to the linked list
+     ******************************************************************************************************************/
     public void add(Auto s) {
 
         //  Order is: (First) List all Cars in bought by date order
@@ -26,42 +52,52 @@ public class MySingleLinkedList implements Serializable
         size++;
         Node temp = top;
 
-        // no list just add
+        // if the list is empty just add
         if (top == null) {
 //            top.setData(s);
 //            top.setNext(null);
             top = new Node(s, null);
             return;
+
+        //if the list is not empty
         }else {
+
             //check the top first, if is car
             if(top.getData() instanceof Car ) {
+
                 //then check the type of s, which you want to add
                 //if s is car, then compare the date
                 if(s instanceof Car){
+
                     //if the top is after s, just add to the top
                     if (top.getData().getBoughtOn().after(s.getBoughtOn())) {
                         top = new Node(s, temp);
                         return;
+
                         //else use while loop to loop through the list, then add
                     }else{
                         while(top.getNext() != null && !top.getNext().getData().getBoughtOn().after(s.getBoughtOn()) ){
                             top = top.getNext();
                             temp = top;
                         }
+
                         //means this is not the last one
                         if(top.getNext() != null)
                             top = new Node(s, temp);
-                            //this is the last one
+
+                        //this is the last one
                         else
                             top.setNext(new Node(s, null));
                         return;
                     }
+
                     //if the type of s is truck, just use the while loop to loop through car
                 }else{
                     while(top.getNext() != null && top.getNext().getData() instanceof Car ){
                         top = top.getNext();
                         temp = top;
                     }
+
                     //if current is not tail, then tail is truck
                     if(top.getNext() != null){
                         //now it is truck
